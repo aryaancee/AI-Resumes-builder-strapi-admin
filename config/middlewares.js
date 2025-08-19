a5db04514a1d769b574ffb776a642f148e846f89
aryaancee <aryanrohatgi51@gmail.com>
2025-08-20 00:12:10 +0530

    Configured CORS for Netlify frontend

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:5175',              // local dev frontend
+        'https://buildresumebyai.netlify.app' // your Netlify production frontend
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
